53443a59a1a4843637b69d949d14469e2ef0129b
Oscar Peng <sitexa@gmail.com>
2015-09-02 16:05:33 +0800

    New changelist

---
+++ presentation/src/main/java/com/sitexa/android/presentation/internal/di/modules/ApplicationModule.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (C) 2015 Fernando Cejas Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.sitexa.android.presentation.internal.di.modules;
+
+import android.content.Context;
+import com.sitexa.android.data.cache.UserCache;
+import com.sitexa.android.data.cache.UserCacheImpl;
+import com.sitexa.android.data.executor.JobExecutor;
+import com.sitexa.android.data.repository.UserDataRepository;
+import com.sitexa.android.domain.executor.PostExecutionThread;
+import com.sitexa.android.domain.executor.ThreadExecutor;
+import com.sitexa.android.domain.repository.UserRepository;
+import com.sitexa.android.presentation.AndroidApplication;
+import com.sitexa.android.presentation.UIThread;
+import com.sitexa.android.presentation.navigation.Navigator;
+import dagger.Module;
+import dagger.Provides;
+import javax.inject.Singleton;
+
+/**
+ * Dagger module that provides objects which will live during the application lifecycle.
+ */
+@Module
+public class ApplicationModule {
+  private final AndroidApplication application;
+
+  public ApplicationModule(AndroidApplication application) {
+    this.application = application;
+  }
+
+  @Provides @Singleton Context provideApplicationContext() {
+    return this.application;
+  }
+
+  @Provides @Singleton Navigator provideNavigator() {
+    return new Navigator();
+  }
+
+  @Provides @Singleton ThreadExecutor provideThreadExecutor(JobExecutor jobExecutor) {
+    return jobExecutor;
+  }
+
+  @Provides @Singleton PostExecutionThread providePostExecutionThread(UIThread uiThread) {
+    return uiThread;
+  }
+
+  @Provides @Singleton UserCache provideUserCache(UserCacheImpl userCache) {
+    return userCache;
+  }
+
+  @Provides @Singleton UserRepository provideUserRepository(UserDataRepository userDataRepository) {
+    return userDataRepository;
+  }
+}
---
+++ presentation/src/main/java/com/sitexa/android/presentation/view/adapter/UsersLayoutManager.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright (C) 2014 android.org. All rights reserved.
+ * @author Fernando Cejas (the android coder)
+ */
+package com.sitexa.android.presentation.view.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
+
+/**
+ * Layout manager to position items inside a {@link android.support.v7.widget.RecyclerView}.
+ */
+public class UsersLayoutManager extends LinearLayoutManager {
+  public UsersLayoutManager(Context context) {
+    super(context);
+  }
+}
